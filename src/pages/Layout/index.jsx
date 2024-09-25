ce5698d02c62b4292c6d7e0805c2351af8b96d4d
Yuting Hu <yuting.hu.careers@gmail.com>
2024-09-25 01:40:28 -0700

    retrirve token test

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { request } from '../../utils'
 
 const Layout = () => {
+ useEffect(()=>{
+  //test if token is retrieve from the request
+  request.get('/user/profile')
+ },[])
   return (
     <div>Layout</div>
   )
