959fe2343cd424b486115f0ca58f3578c98d1019
Yuting Hu <yuting.hu.careers@gmail.com>
2024-10-10 16:04:49 -0700

    implementation of lazy loading

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,15 +1,16 @@
-
-import React from 'react'
 import { createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from '@/pages/Layout'
 import Login from '@/pages/Login'
 import AuthRoute from '@/components/AuthRoute'
-import Home from '../pages/Home'
-import Article from '../pages/Article'
-import Publish from '../pages/Publish'
+// import Home from '../pages/Home'
+// import Article from '../pages/Article'
+// import Publish from '../pages/Publish'
 import { Route } from 'react-router-dom'
+import { lazy, Suspense } from 'react'
 
-
+const Home = lazy(() => import('@/pages/Home'))
+const Article = lazy(() => import('@/pages/Article'))
+const Publish = lazy(() => import('@/pages/Publish'))
 // const router = createBrowserRouter([
 //   {
 //     path: "/",
@@ -39,11 +40,20 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<AuthRoute> <Layout /> </AuthRoute>}>
-        <Route index element={<Home />} />
-        <Route path="home" element={<Home />} />
-        <Route path="article" element={<Article />} />
-        <Route path="publish" element={<Publish />} />
-      </Route>
+        <Route index element={
+          <Suspense fallback={'Loading...'}><Home /></Suspense>
+        } />
+        <Route path="home" element={
+          <Suspense fallback={'Loading...'}><Home /></Suspense>
+        } />
+        <Route path="article" element={
+          <Suspense fallback={'Loading...'}><Article /></Suspense>
+        } />
+        <Route path="publish" element={
+          <Suspense fallback={'Loading...'}><Publish /></Suspense>
+        } />
+      </Route >
+      
       <Route path="/login" element={<Login />} />
     </>
   )
